9e25952b0b00cd8713d2ee9ec65adc5f4e5a8034
BinBin-27 <bintangaul27@gmail.com>
2022-09-01 14:26:53 +0700

    esdr

--- src/App.js
+++ src/App.js
@@ -2,30 +2,21 @@ import Home from "./pages/Home";
 import Login from "./pages/Login"
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import Biodata from "./pages/Biodata";
-import Tutorial_A from "./pages/Tutorials/Tutorial_A"
-import Question_A from "./pages/Questions/Question_A";
-import Tutorial_B from "./pages/Tutorials/Tutorial_B"
-import Question_B from "./pages/Questions/Question_B";
-import Tutorial_C from "./pages/Tutorials/Tutorial_C"
-import Question_C from "./pages/Questions/Question_C";
-import Tutorial_D from "./pages/Tutorials/Tutorial_D"
-import Question_D from "./pages/Questions/Question_D";
-import Tutorial_E from "./pages/Tutorials/Tutorial_E"
-import Question_E from "./pages/Questions/Question_E";
-import Tutorial_F from "./pages/Tutorials/Tutorial_F"
-import Question_F from "./pages/Questions/Question_F";
-import Tutorial_G from "./pages/Tutorials/Tutorial_G"
-import Question_G from "./pages/Questions/Question_G";
-import Tutorial_H from "./pages/Tutorials/Tutorial_H"
-import Question_H from "./pages/Questions/Question_H";
-import Tutorial_I from "./pages/Tutorials/Tutorial_I"
-import Question_I from "./pages/Questions/Question_I";
+import Tutorial_A from "./pages/Tutorials/Tutorial_A";import Question_A from "./pages/Questions/Question_A";
+import Tutorial_B from "./pages/Tutorials/Tutorial_B";import Question_B from "./pages/Questions/Question_B";
+import Tutorial_C from "./pages/Tutorials/Tutorial_C";import Question_C from "./pages/Questions/Question_C";
+import Tutorial_D from "./pages/Tutorials/Tutorial_D";import Question_D from "./pages/Questions/Question_D";
+import Tutorial_E from "./pages/Tutorials/Tutorial_E";import Question_E from "./pages/Questions/Question_E";
+import Tutorial_F from "./pages/Tutorials/Tutorial_F";import Question_F from "./pages/Questions/Question_F";
+import Tutorial_G from "./pages/Tutorials/Tutorial_G";import Question_G from "./pages/Questions/Question_G";
+import Tutorial_H from "./pages/Tutorials/Tutorial_H";import Question_H from "./pages/Questions/Question_H";
+import Tutorial_I from "./pages/Tutorials/Tutorial_I";import Question_I from "./pages/Questions/Question_I";
 import LastPage from "./pages/LastPage"
 import Table from "./pages/Table"
 import background from ".//bg.png"
 
 function App() {
- 
+  
   return (
     <div className="w-full flex justify-center" id="home">
     <div className="w-full h-screen flex flex-col items-center justify-center"
